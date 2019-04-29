edced4414d81d16a6d5318b227871cf967369f46
Phil Tanner <phil@philtanner.com>
2019-04-30 09:00:38 +1200

    Change check locations for backups. Remove inaccessible folders from git checks

--- check_backup_database_daily.sh
+++ check_backup_database_daily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/lib/nagios/plugins/check_file_age -f /media/backup_usb/backups/weekly/`date "+%a"`/mysql.dmp.gpg -w $1 -c $2 -W $3 -C $4
+/usr/lib/nagios/plugins/check_file_age -f /media/backup_usb/backups/weekly/`date "+%a"`/dreamhost/sql_backups/all_db.sql.gpg -w $1 -c $2 -W $3 -C $4
--- check_backup_database_monthly.sh
+++ check_backup_database_monthly.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/lib/nagios/plugins/check_file_age -f /media/backup_usb/backups/monthly/`date "+%b"`/mysql.dmp.gpg -w $1 -c $2 -W $3 -C $4
+/usr/lib/nagios/plugins/check_file_age -f /media/backup_usb/backups/monthly/`date "+%b"`/dreamhost/sql_backups/all_db.sql.gpg -w $1 -c $2 -W $3 -C $4
